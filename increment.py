1037e834e394fdbad9c1063909c31eea37a1f54a
yanbin_yb <yanbin.yb@hotmail.com>
2023-12-05 12:40:47 +0000

    update increment.py.
    Signed-off-by: yanbin_yb <yanbin.yb@hotmail.com>

--- increment.py
+++ increment.py
@@ -7,13 +7,16 @@ import mysqlutils
 filter = b'test.pl_branch'
 
 client = Client()
-client.connect(host='127.0.0.1', port=11111)  
-client.check_valid(username=b'', password=b'')  
+client.connect(host='127.0.0.1', port=11111)
+client.check_valid(username=b'', password=b'')
 client.subscribe(client_id=b'1001', destination=b'example',
                  filter=filter)
 
 target = mysqlutils.MySQL(host="127.0.0.1", user="root",
                           password="root", database="test2", port=3306)
+
+
+split_table = 2
 
 while True:
     message = client.get(100)
@@ -36,9 +39,11 @@ while True:
         for row in row_change.rowDatas:
             format_data = dict()
             if event_type == EntryProtocol_pb2.EventType.DELETE:
-                target.delete(table_name=table, id=row.beforeColumns[0].value)
+                mytable = table+str(row.beforeColumns[0].value % split_table)
+                target.delete(table_name=mytable, id=row.beforeColumns[0].value)
             elif event_type == EntryProtocol_pb2.EventType.INSERT or event_type == EntryProtocol_pb2.EventType.UPDATE:
-                target.update(table_name=table, data=row.afterColumns)
+                mytable = table+str(int(row.afterColumns[0].value) % split_table)
+                target.update(table_name=mytable, data=row.afterColumns)
             else:
                 None
     time.sleep(1)
